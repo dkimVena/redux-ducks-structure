0b85b3e3e568c8de280454eb65efc615908b3b88
goginim <david.ingook@gmail.com>
2018-11-07 18:56:52 -0500

    basic template

---
+++ src/store/modules/counter.js
@@ -0,0 +1,23 @@
+import { createAction, handleActions } from 'redux-action';
+
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+export const increment = createAction(INCREMENT);
+export const decrement = createAction(DECREMENT);
+
+const initialState = {
+  number: 0
+};
+
+export default handleActions(
+  {
+    [INCREMENT]: (state, action) => {
+      return { number: state.number + 1 };
+    },
+    [DECREMENT]: ({ number }) => {
+      return { number: number - 1 };
+    }
+  },
+  initialState
+);
